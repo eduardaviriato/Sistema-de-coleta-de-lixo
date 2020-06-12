cef7194fdb11a6cd0b25d81374965d28a4e32e20
eduardaviriato <51841232+eduardaviriato@users.noreply.github.com>
2020-06-11 23:32:51 -0300

    Add files via upload

---
+++ NLW/scripts/create-point.js
@@ -0,0 +1,98 @@
+
+function populateUFs(){
+    const ufSelect =document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res)=>{return res.json()})
+    .then (states =>{
+
+        for(const state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+
+        }
+
+    })
+
+
+}
+
+populateUFs()
+
+function getCities(event){
+    const citySelect =document.querySelector("select[name=city]")
+    const stateInput =document.querySelector("input[name=state]")
+
+    const indexOfSelectedState = event.target.selectedIndex
+    const ufvalue= event.target.value
+
+    stateInput.value =event.target.options[indexOfSelectedState].text
+
+
+    const url =`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufvalue}/municipios`
+
+    citySelect.innerHTML ="<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+
+
+    fetch(url)
+    .then((res)=>{return res.json()})
+    .then (cities =>{
+
+        for(const city of cities){
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+
+        }
+
+        citySelect.disabled = false
+
+    })
+
+}
+
+document
+//seleciona um campo
+    .querySelector("select[name=uf]")
+//    //ouvindo um evento, evento da pagina carregando, mudança...
+    .addEventListener("change", getCities)
+
+//Itens de coleta
+//pegar todos os li`s
+const itensToCollect= document.querySelectorAll(".itens-grid li")
+for (const item of itensToCollect){
+    item.addEventListener("click", handleSelectedItem)
+
+}
+
+const collectItens = document.querySelector("input[name=itens]")
+
+
+let selectItens = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+    const alreadySelected = selectItens.findIndex(function(item){
+        const itemFound = item == itemId //isso será true ou false
+        return itemFound
+    })
+
+    //se já estiver selecionado, tirar da seleção
+    if(alreadySelected >= 0){
+        // tirar da seleção
+        const filteredItens = selectItens.filter( item =>{
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectItens = filteredItens
+    } else{
+    //se não estiver selecionado, adicionar a seleção
+    selectItens.push(itemId)
+    }
+    //atualizar o campo escondido com os itens selecionados
+    collectItens.value = selectItens
+}
